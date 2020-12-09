3ec58c2ffe76b5062c67d59a1d910eac69adb953
Max <maksymilianrozanski@gmail.com>
2020-12-09 18:22:41 +0100

    add ShoppingListCreateDto to ShoppingList conversion

--- ShoppingList/Entities/ShoppingListEntity.cs
+++ ShoppingList/Entities/ShoppingListEntity.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using Microsoft.FSharp.Collections;
 using ShoppingData;
+using ShoppingList.Dtos;
 
 namespace ShoppingList.Entities
 {
@@ -14,5 +15,14 @@ namespace ShoppingList.Entities
         [Required] public string Password { get; set; }
 
         [Required] public IEnumerable<ItemDataEntity> Items { get; set; }
+
+        public static implicit operator ShoppingListEntity(ShoppingListCreateDto createDto) =>
+            new ShoppingListEntity
+            {
+                Id = 0,
+                Name = createDto.Name,
+                Password = createDto.Password,
+                Items = new List<ItemDataEntity>()
+            };
     }
 }
