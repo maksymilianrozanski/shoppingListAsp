2a15ab0b22990b24f8265f40a98e71a3b18840ce
Max <maksymilianrozanski@gmail.com>
2020-12-09 15:30:06 +0100

    add ItemDataEntity tests

--- ShoppingListTests/Entities/ItemDataEntityTests.cs
+++ ShoppingListTests/Entities/ItemDataEntityTests.cs
@@ -1,4 +1,5 @@
-using System;
+using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
 using ShoppingData;
 using ShoppingList.Entities;
@@ -15,38 +16,50 @@ namespace ShoppingListTests.Entities
         [Test]
         public void ShouldConvertItemDataToItemDataEntity()
         {
-            var itemData = new ShoppingItemModule.ItemData(10, "sugar", 12, ShoppingItemModule.ItemType.Bought);
-            var expected = new ItemDataEntity
+            var itemsToConvert = new List<ShoppingItemModule.ItemData>
             {
-                Id = 10, Name = "sugar", Quantity = 12, ItemType = "Bought"
+                new ShoppingItemModule.ItemData(10, "sugar", 1, ShoppingItemModule.ItemType.Bought),
+                new ShoppingItemModule.ItemData(11, "coffee", 20, ShoppingItemModule.ItemType.NewAssigned("Garfield")),
+                new ShoppingItemModule.ItemData(12, "milk", 21, ShoppingItemModule.ItemType.ToBuy),
+                new ShoppingItemModule.ItemData(13, "apples", 22, ShoppingItemModule.ItemType.Cancelled),
+                new ShoppingItemModule.ItemData(14, "pineapple", 23, ShoppingItemModule.ItemType.NotFound),
             };
 
-            var result = itemData;
+            var expectedItems = new List<ItemDataEntity>
+            {
+                new ItemDataEntity {Id = 10, Name = "sugar", Quantity = 1, ItemType = "Bought"},
+                new ItemDataEntity {Id = 11, Name = "coffee", Quantity = 20, ItemType = "Assigned Garfield"},
+                new ItemDataEntity {Id = 12, Name = "milk", Quantity = 21, ItemType = "ToBuy"},
+                new ItemDataEntity {Id = 13, Name = "apples", Quantity = 22, ItemType = "Cancelled"},
+                new ItemDataEntity {Id = 14, Name = "pineapple", Quantity = 23, ItemType = "NotFound"}
+            };
 
-            AreEqual(expected, result);
+            Assert.True(AreAllEqual(expectedItems, itemsToConvert));
         }
 
         [Test]
-        public void ShouldConvertItemDataToItemDataEntity2()
+        public void ConvertedItemShouldNotBeEqual()
         {
-            var itemData =
-                new ShoppingItemModule.ItemData(10, "sugar", 12, ShoppingItemModule.ItemType.NewAssigned("Garfield"));
-            var expected = new ItemDataEntity
-            {
-                Id = 10, Name = "sugar", Quantity = 12, ItemType = "Assigned Garfield"
-            };
+            var item = new ShoppingItemModule.ItemData(11, "coffee", 20,
+                ShoppingItemModule.ItemType.NewAssigned("Garfield"));
 
-            var result = itemData;
-
-            AreEqual(expected, result);
+            var notEqual = new ShoppingItemModule.ItemData(11, "coffee", 20, ShoppingItemModule.ItemType.ToBuy);
+            Assert.AreNotEqual(item, notEqual);
         }
 
-        private static void AreEqual(ItemDataEntity expected, ItemDataEntity result)
+        private static bool AreAllEqual(IEnumerable<ItemDataEntity> expectedItems,
+            IEnumerable<ShoppingItemModule.ItemData> actualItems) =>
+            expectedItems.Zip(actualItems)
+                .Select(i => EntitiesEqual(i))
+                .All(i => i);
+
+        private static bool EntitiesEqual((ItemDataEntity, ItemDataEntity) entities)
         {
-            Assert.AreEqual(expected.Id, result.Id, "Id values should be equal");
-            Assert.AreEqual(expected.Name, result.Name, "Name values should be equal");
-            Assert.AreEqual(expected.Quantity, result.Quantity, "Quantity values should be equal");
-            Assert.AreEqual(expected.ItemType, result.ItemType, "ItemType values should be equal");
+            var (expected, actual) = entities;
+            return expected.Id == actual.Id &&
+                   expected.Name == actual.Name &&
+                   expected.Quantity == actual.Quantity &&
+                   expected.ItemType == actual.ItemType;
         }
     }
 }
