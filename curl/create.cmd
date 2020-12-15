a541150bd21e32eb43e86ebaed53928e59b22927
Max <maksymilianrozanski@gmail.com>
2020-12-15 20:51:41 +0100

    update create.cmd request url path

--- curl/create.cmd
+++ curl/create.cmd
@@ -1,2 +1,2 @@
 rem requests adding new shopping list
-curl -X POST "localhost:5000/shoppingList" -d "@json/shoppingList.json" -H "Content-Type: application/json" -v --ssl-no-revoke
+curl -X POST "localhost:5000/shoppingList/createList" -d "@json/shoppingList.json" -H "Content-Type: application/json" -v --ssl-no-revoke
