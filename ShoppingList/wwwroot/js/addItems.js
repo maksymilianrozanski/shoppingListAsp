297212aa2c65a48729569d8af76f75b87edc3f0d
Max <maksymilianrozanski@gmail.com>
2020-12-16 20:42:13 +0100

    display list of saved items

--- ShoppingList/wwwroot/js/addItems.js
+++ ShoppingList/wwwroot/js/addItems.js
@@ -27,10 +27,47 @@
                 if (r.status === 200) {
                     // 3. un-hide the alertElement when the request has been successful
                     console.log("success");
+                    fetchUpdatedView();
                 }
             }
         )
     };
+
+    const displayListItems = async (responseJson) => {
+        let response = await responseJson;
+        console.log("displaying not implemented yet.");
+        console.log("responseJson: " + response.id);
+        console.log("name: " + response.name);
+        let items = Array.from(response.items);
+
+        let savedItems = document.getElementById("savedItemsList");
+        savedItems.innerHTML = "";
+        items.map(i => {
+            let newListItem = document.createElement("li");
+            newListItem.innerText = "id: " + i.id.toString() + ", " + i.name.toString() + " - " + i.quantity.toString();
+            return newListItem;
+        }).forEach(i => savedItems.appendChild(i));
+    }
+
+    const fetchUpdatedView = async () => {
+        await fetch("/shoppingList/" + formElement.elements[0].value, {
+            method: 'GET',
+            mode: 'cors', // no-cors, *cors, same-origin
+            cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: 'same-origin', // include, *same-origin, omit
+            headers: {
+                'Accept': 'application/json'
+            },
+            redirect: 'follow',
+            referrerPolicy: 'no-referrer',
+        }).then(r => {
+            if (r.status === 200) {
+                console.log("fetching shopping list data successful");
+                let responseJson = r.json();
+                displayListItems(responseJson)
+            }
+        })
+    }
 
     window.addEventListener("load", () => {
         formElement.addEventListener("submit", event => {
