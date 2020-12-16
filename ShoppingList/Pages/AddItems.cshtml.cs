9ddf530c8649faea8ffacdc6a08fd09570c9510f
Max <maksymilianrozanski@gmail.com>
2020-12-16 18:46:36 +0100

    add ShoppingListCreate.cshtml

---
+++ ShoppingList/Pages/AddItems.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ShoppingList.Pages
+{
+    public class AddItems : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
