a298b15c2b5d065933c7c77a12725e7a7f98cd6e
Max <maksymilianrozanski@gmail.com>
2020-12-17 14:15:56 +0100

    add buttons to displayed items at /collectingItems

--- ShoppingList/wwwroot/js/collectingItems.js
+++ ShoppingList/wwwroot/js/collectingItems.js
@@ -1,54 +1,80 @@
 (function () {
-    const alertElement = document.getElementById("success-alert");
-    const shoppingListNameAndPassword = document.forms[0];
+        const alertElement = document.getElementById("success-alert");
+        const shoppingListNameAndPassword = document.forms[0];
 
-    const itemActionsIds = {
-        AssignItem: 0,
-        ItemToNotFound: 1,
-        ItemToBought: 2,
-        ItemToCancelled: 3
-    }
+        const itemActionsIds = {
+            AssignItem: 0,
+            ItemToNotFound: 1,
+            ItemToBought: 2,
+            ItemToCancelled: 3
+        }
 
-    const getAllowedActions = (actionText) => {
-        if (actionText.startsWith("ToBuy"))
-            return [itemActionsIds.AssignItem, itemActionsIds.ItemToCancelled];
-        else if (actionText.startsWith("Assigned"))
-            return [itemActionsIds.ItemToBought, itemActionsIds.ItemToNotFound, itemActionsIds.ItemToCancelled];
-        else if (actionText.startsWith("Bought"))
-            return [];
-        else if (actionText.startsWith("NotFound"))
+        const getAllowedActions = (itemType) => {
+            if (itemType.startsWith("ToBuy"))
                 return [itemActionsIds.AssignItem, itemActionsIds.ItemToCancelled];
-        else if (actionText.startsWith("Cancelled"))
-            return [];
-    }
-    
-    const itemWithActions = (itemDataReadDto) => {
-        const liElement = document.createElement("li");
-        const displayedText = document.createElement("a")
-        displayedText.innerText = "id: " + itemDataReadDto.id.toString() + ", " + itemDataReadDto.name.toString() + " - " + itemDataReadDto.quantity.toString();
-        liElement.appendChild(displayedText);
-        const allowedActionsTextForDto = getAllowedActions(itemDataReadDto.itemType.toString())
-        const allowedActionsElement = document.createElement("div")
-        allowedActionsElement.innerText = allowedActionsTextForDto.toString();
-        liElement.appendChild(allowedActionsElement)
-        return liElement;
-    }
+            else if (itemType.startsWith("Assigned"))
+                return [itemActionsIds.ItemToBought, itemActionsIds.ItemToNotFound, itemActionsIds.ItemToCancelled];
+            else if (itemType.startsWith("Bought"))
+                return [];
+            else if (itemType.startsWith("NotFound"))
+                return [itemActionsIds.AssignItem, itemActionsIds.ItemToCancelled];
+            else if (itemType.startsWith("Cancelled"))
+                return [];
+        }
 
-    const displayItems = async (items) => {
-        const olRoot = document.getElementById("shoppingListItems");
-        olRoot.innerHTML = "";
+        const itemActionText = (actionId) => {
+            switch (actionId) {
+                case itemActionsIds.AssignItem:
+                    return "Assign item";
+                case itemActionsIds.ItemToNotFound:
+                    return "Not found";
+                case itemActionsIds.ItemToBought:
+                    return "Item found";
+                case itemActionsIds.ItemToCancelled:
+                    return "Cancel item";
+            }
+        }
 
-        Array.from(items)
-            .map(i => itemWithActions(i))
-            .forEach(i => olRoot.appendChild(i));
-    }
+        const createItemActionButton = (itemDataReadDto) => (actionId) => {
+            const actionButton = document.createElement("button")
+            actionButton.innerText = itemActionText(actionId);
+            //todo: add sending data on button click
+            return actionButton;
+        }
 
-    window.addEventListener("load", () => {
-        shoppingListNameAndPassword.addEventListener("submit", event => {
-            event.preventDefault();
-            fetchShoppingList(shoppingListNameAndPassword.elements[0].value)
-                .then(r => displayItems(r.items));
+        const itemButtons = (itemDataReadDto) => {
+            const itemType = itemDataReadDto.itemType;
+            const actions = Array.from(getAllowedActions(itemType));
+            return actions.map(i => createItemActionButton(itemDataReadDto)(i));
+        }
+
+        const itemWithActions = (itemDataReadDto) => {
+            const liElement = document.createElement("li");
+
+            const displayedText = document.createElement("a")
+            displayedText.innerText = "id: " + itemDataReadDto.id.toString() + ", " + itemDataReadDto.name.toString() + " - " + itemDataReadDto.quantity.toString();
+            liElement.appendChild(displayedText);
+
+            itemButtons(itemDataReadDto).forEach(i => liElement.appendChild(i));
+            return liElement;
+        }
+
+        const displayItems = async (items) => {
+            const olRoot = document.getElementById("shoppingListItems");
+            olRoot.innerHTML = "";
+
+            Array.from(items)
+                .map(i => itemWithActions(i))
+                .forEach(i => olRoot.appendChild(i));
+        }
+
+        window.addEventListener("load", () => {
+            shoppingListNameAndPassword.addEventListener("submit", event => {
+                event.preventDefault();
+                fetchShoppingList(shoppingListNameAndPassword.elements[0].value)
+                    .then(r => displayItems(r.items));
+            });
         });
-    });
-})
+    }
+)
 ();
