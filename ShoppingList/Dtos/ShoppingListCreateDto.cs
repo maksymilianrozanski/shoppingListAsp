a98e52c58c0ff0e16e14b893fe288d13c939090b
Max <maksymilianrozanski@gmail.com>
2020-12-29 12:42:12 +0100

    add ShoppingListCreate2.cshtml

--- ShoppingList/Dtos/ShoppingListCreateDto.cs
+++ ShoppingList/Dtos/ShoppingListCreateDto.cs
@@ -7,6 +7,12 @@ namespace ShoppingList.Dtos
 {
     public class ShoppingListCreateDto
     {
+        public ShoppingListCreateDto(string shoppingListName, string password)
+        {
+            Name = shoppingListName;
+            Password = password;
+        }
+
         [Required] [MaxLength(100)] public string Name { get; set; }
         [Required] [MaxLength(20)] public string Password { get; set; }
     }
---
+++ ShoppingList/Pages/ShoppingListCreate2.cshtml.cs
@@ -0,0 +1,48 @@
+using System;
+using LaYumba.Functional;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ShoppingList.Auth;
+using ShoppingList.Data;
+using ShoppingList.Dtos;
+
+namespace ShoppingList.Pages
+{
+    public class ShoppingListCreate2 : PageModel
+    {
+        [BindProperty] public string ShoppingListName { get; set; }
+
+        [BindProperty] public string Username { get; set; }
+
+        [BindProperty] public string Password { get; set; }
+
+        private readonly BasicAuthenticationHandler _authenticationHandler;
+        private readonly IShoppingListRepo _repository;
+
+        public ShoppingListCreate2(BasicAuthenticationHandler authenticationHandler, IShoppingListRepo repository)
+        {
+            _authenticationHandler = authenticationHandler;
+            _repository = repository;
+        }
+
+        public void OnGet()
+        {
+        }
+
+        public void OnPost()
+        {
+            new ShoppingListCreateDto(ShoppingListName, Password).Pipe(createDto =>
+                _repository.CreateShoppingList(createDto)
+                    .Match(() => throw new NotImplementedException(), readDto =>
+                        _authenticationHandler
+                            .CreateClaims(new BasicAuthenticationHandler.UserLoginData(readDto.Id,
+                                Username,
+                                createDto.Password))
+                            .Map(c => HttpContext.SignInAsync("CookieAuthentication", c))
+                            .Run()
+                            .Match(l => throw new NotImplementedException(),
+                                r => Response.Redirect("/Protected/AddItems"))));
+        }
+    }
+}
