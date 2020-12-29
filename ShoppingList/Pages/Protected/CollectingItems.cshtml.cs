69dba04921480eb049a9892f6c79c2237118dc46
Max <maksymilianrozanski@gmail.com>
2020-12-29 10:56:59 +0100

    rename CollectingItems2.cshtml

--- ShoppingList/Pages/Protected/CollectingItems.cshtml.cs
+++ ShoppingList/Pages/Protected/CollectingItems.cshtml.cs
@@ -6,25 +6,27 @@ using static ShoppingList.Auth.BasicAuthenticationHandler.User;
 
 namespace ShoppingList.Pages.Protected
 {
-    public class CollectingItems2 : PageModel
+    public class CollectingItems : PageModel
     {
         private readonly IShoppingListRepo _shoppingListRepo;
-        
+
         public string CurrentUsername { get; private set; }
         public int CurrentShoppingListId { get; private set; }
 
-        public CollectingItems2(IShoppingListRepo shoppingListRepo)
+        public CollectingItems(IShoppingListRepo shoppingListRepo)
         {
             _shoppingListRepo = shoppingListRepo;
+            CurrentUsername = "";
+            RefreshUserData();
         }
 
-        public void OnGet()
-        {
+        public void OnGet() => RefreshUserData();
+
+        private void RefreshUserData() =>
             ToOptionUser(HttpContext).ForEach(i =>
             {
                 CurrentUsername = i.Username;
                 CurrentShoppingListId = i.ShoppingListId;
             });
-        }
     }
 }
