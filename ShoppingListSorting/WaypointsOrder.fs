858537a5851b3b350805002b8fc90feae12e2100
Max <maksymilianrozanski@gmail.com>
2021-01-07 16:19:18 +0100

    change return type of distinctAndDefinedWaypoints to 'Waypoint list', add sortWaypoints function

--- ShoppingListSorting/WaypointsOrder.fs
+++ ShoppingListSorting/WaypointsOrder.fs
@@ -1,5 +1,6 @@
 namespace ShoppingListSorting
 
+open FSharpPlus.Control
 open ShoppingListSorting.ShoppingItemsAddWaypoints
 
 open Waypoints
@@ -13,11 +14,18 @@ module WaypointsOrder =
              | (_, _, w) -> w)) shoppingList.Items
         |> List.filter (fun x -> x.IsSome)
         |> List.distinct
+        |> List.choose id
 
     let private indexDictionary (items: list<'a>) =
         List.zip items ({ 0 .. items.Length } |> Seq.toList)
         |> dict
 
-//    let sortWaypoints (shoppingList: ShoppingListWithWaypoints) =
-//        let waypointsWithStartAndEnd =
-//            shoppingList.
+    let sortWaypoints (shoppingList: ShoppingListWithWaypoints) =
+        match shoppingList.StartAndCheckout with
+        | (start, checkout) ->
+            [ start ]
+            @ (distinctAndDefinedWaypoints shoppingList)
+              @ [ checkout ]
+        |> Array.ofList
+        |> waypointNamesSorted
+        |> fun x -> x.Tail
--- ShoppingListSortingTests/WaypointsOrderTests.fs
+++ ShoppingListSortingTests/WaypointsOrderTests.fs
@@ -1,61 +1,68 @@
 module ShoppingListSortingTests.WaypointsOrderTests
 
 open NUnit.Framework
+open NUnit.Framework
 open ShoppingData.ShoppingItemModule
 open ShoppingList.Dtos
 open ShoppingListSorting.ShoppingItemsPredicting
 open ShoppingListSorting
 open ShoppingListSorting.ShoppingItemsAddWaypoints
 open Waypoints.WaypointsModule
+open WaypointsOrder
 
 [<SetUp>]
 let Setup () = ()
 
+let waypoints =
+    ShopWaypointsReadDto
+        ("big-market",
+         { name = "start"; x = 0L; y = 0L },
+         { name = "checkout"
+           x = 100L
+           y = 100L },
+         [ { name = "JUICES"; x = 10L; y = 10L }
+           { name = "BREAD"; x = 20L; y = 20L }
+           { name = "CHEESE"; x = 30L; y = 30L } ]
+         |> ResizeArray)
+
+let shoppingList: ShoppingListWithWaypoints =
+    { Id = 0
+      Name = "My-shopping-list"
+      Password = "pass"
+      ShopName = "todo"
+      Items =
+          [ ({ Id = 0
+               Name = "Orange juice"
+               Quantity = 1
+               ItemType = ItemType.ToBuy },
+             PredictedShopsDepartment("JUICES"),
+             Some(waypoints.Waypoints.Item(0)))
+            ({ Id = 1
+               Name = "Carrot"
+               Quantity = 2
+               ItemType = ItemType.ToBuy },
+             PredictedShopsDepartment("VEGETABLES"),
+             None)
+            ({ Id = 2
+               Name = "Cheddar cheese"
+               Quantity = 1
+               ItemType = ItemType.ToBuy },
+             PredictedShopsDepartment("CHEESE"),
+             Some(waypoints.Waypoints.Item(2))) ]
+      StartAndCheckout = (waypoints.Start, waypoints.Checkout) }
+
 [<Test>]
 let ``should return list of distinct waypoints`` () =
-    let waypoints =
-        ShopWaypointsReadDto
-            ("big-market",
-             { name = "start"; x = 0L; y = 0L },
-             { name = "checkout"
-               x = 100L
-               y = 100L },
-             [ { name = "JUICES"; x = 10L; y = 10L }
-               { name = "BREAD"; x = 20L; y = 20L }
-               { name = "CHEESE"; x = 30L; y = 30L } ]
-             |> ResizeArray)
-
-    let shoppingList: ShoppingListWithWaypoints =
-        { Id = 0
-          Name = "My-shopping-list"
-          Password = "pass"
-          ShopName = "todo"
-          Items =
-              [ ({ Id = 0
-                   Name = "Orange juice"
-                   Quantity = 1
-                   ItemType = ItemType.ToBuy },
-                 PredictedShopsDepartment("JUICES"),
-                 Some(waypoints.Waypoints.Item(0)))
-                ({ Id = 1
-                   Name = "Carrot"
-                   Quantity = 2
-                   ItemType = ItemType.ToBuy },
-                 PredictedShopsDepartment("VEGETABLES"),
-                 None)
-                ({ Id = 2
-                   Name = "Cheddar cheese"
-                   Quantity = 1
-                   ItemType = ItemType.ToBuy },
-                 PredictedShopsDepartment("CHEESE"),
-                 Some(waypoints.Waypoints.Item(2))) ]
-          StartAndCheckout = (waypoints.Start, waypoints.Checkout) }
-
     let expected =
-        [ Some(waypoints.Waypoints.Item(0))
-          Some(waypoints.Waypoints.Item(2)) ]
+        [ waypoints.Waypoints.Item(0)
+          waypoints.Waypoints.Item(2) ]
 
-    let result =
-        WaypointsOrder.distinctAndDefinedWaypoints shoppingList
+    let result = distinctAndDefinedWaypoints shoppingList
 
+    Assert.AreEqual(expected, result)
+
+[<Test>]
+let ``should return list of waypoints sorted in expected order`` () =
+    let expected = [ "JUICES"; "CHEESE" ]
+    let result = sortWaypoints shoppingList
     Assert.AreEqual(expected, result)
