810fd2a5110f0acd38e17e6ecb935bee767fa9c2
Max <maksymilianrozanski@gmail.com>
2021-01-07 12:51:52 +0100

    add addWaypointsToShoppingList

--- ShoppingListSorting/ShoppingItemsAddWaypoints.fs
+++ ShoppingListSorting/ShoppingItemsAddWaypoints.fs
@@ -13,9 +13,29 @@ module ShoppingItemsAddWaypoints =
         match itemData with
         | (_, PredictedShopsDepartment (b)) -> b = waypoint.name
 
-    let pickSingleWaypoint (itemData: ItemDataWithPredictedType) (waypoints: ShopWaypointsReadDto) =
+    let pickSingleWaypoint (waypoints: ShopWaypointsReadDto) (itemData: ItemDataWithPredictedType) =
         (waypoints.Waypoints |> Seq.toList)
         |> List.tryFind (isWaypointMatched <| itemData)
 
-    let pickWaypoints (shoppingList: ShoppingListWithDepartment) (waypoints: ShopWaypointsReadDto) =
-        NotImplementedException
+    type ItemDataWithPredictedTypeWithWaypoint = ItemData * PredictedShopsDepartment * Waypoint option
+
+    let private addWaypointToItemData (waypoints: ShopWaypointsReadDto) (itemData: ItemDataWithPredictedType) =
+        match itemData with
+        | (data, predicted) -> (data, predicted, pickSingleWaypoint waypoints itemData)
+
+    type ShoppingListWithWaypoints =
+        { Id: int
+          Name: string
+          Password: string
+          ShopName: string
+          Items: ItemDataWithPredictedTypeWithWaypoint list }
+
+    let private pickWaypoints (shoppingList: ShoppingListWithDepartment) (waypoints: ShopWaypointsReadDto) =
+        List.map (addWaypointToItemData waypoints) (shoppingList.Items |> Seq.toList)
+
+    let addWaypointsToShoppingList (shoppingList: ShoppingListWithDepartment) (waypoints: ShopWaypointsReadDto) =
+        { Id = shoppingList.Id
+          Name = shoppingList.Name
+          Password = shoppingList.Password
+          ShopName = shoppingList.ShopName
+          Items = pickWaypoints shoppingList waypoints }
--- ShoppingListSortingTests/ShoppingItemsAddWaypointsTests.fs
+++ ShoppingListSortingTests/ShoppingItemsAddWaypointsTests.fs
@@ -3,6 +3,8 @@ module ShoppingListSortingTests.ShoppingItemsAddWaypointsTests
 
 open FSharpPlus
 open NUnit.Framework
+open NUnit.Framework
+open NUnit.Framework
 open NUnit.Framework.Internal
 open ShoppingData
 open ShoppingList.Dtos
@@ -43,6 +45,74 @@ let ``should pick waypoint with matching name`` () =
            ItemType = ItemType.ToBuy },
          PredictedShopsDepartment("BREAD"))
 
-    let result = pickSingleWaypoint itemData waypoints
+    let result = pickSingleWaypoint waypoints itemData
+
+    Assert.AreEqual(expected, result)
+
+[<Test>]
+let ``should pick waypoints for all items`` () =
+    let waypoints =
+        ShopWaypointsReadDto
+            ("big-market",
+             { name = "start"; x = 0L; y = 0L },
+             { name = "checkout"
+               x = 100L
+               y = 100L },
+             [ { name = "JUICES"; x = 10L; y = 10L }
+               { name = "BREAD"; x = 20L; y = 20L }
+               { name = "CHEESE"; x = 30L; y = 30L } ]
+
+             |> ResizeArray)
+
+    let shoppingList: ShoppingListWithDepartment =
+        { Id = 0
+          Name = "My-shopping-list"
+          Password = "pass"
+          ShopName = "todo"
+          Items =
+              ResizeArray<ItemDataWithPredictedType>
+                  [ ({ Id = 0
+                       Name = "Orange juice"
+                       Quantity = 1
+                       ItemType = ItemType.ToBuy },
+                     PredictedShopsDepartment("JUICES"))
+                    ({ Id = 1
+                       Name = "Carrot"
+                       Quantity = 2
+                       ItemType = ItemType.ToBuy },
+                     PredictedShopsDepartment("VEGETABLES"))
+                    ({ Id = 2
+                       Name = "Cheddar cheese"
+                       Quantity = 1
+                       ItemType = ItemType.ToBuy },
+                     PredictedShopsDepartment("CHEESE")) ] }
+
+    let expected: ShoppingListWithWaypoints =
+        { Id = 0
+          Name = "My-shopping-list"
+          Password = "pass"
+          ShopName = "todo"
+          Items =
+              [ ({ Id = 0
+                   Name = "Orange juice"
+                   Quantity = 1
+                   ItemType = ItemType.ToBuy },
+                 PredictedShopsDepartment("JUICES"),
+                 Some(waypoints.Waypoints.Item(0)))
+                ({ Id = 1
+                   Name = "Carrot"
+                   Quantity = 2
+                   ItemType = ItemType.ToBuy },
+                 PredictedShopsDepartment("VEGETABLES"),
+                 None)
+                ({ Id = 2
+                   Name = "Cheddar cheese"
+                   Quantity = 1
+                   ItemType = ItemType.ToBuy },
+                 PredictedShopsDepartment("CHEESE"),
+                 Some(waypoints.Waypoints.Item(2))) ] }
+
+    let result =
+        addWaypointsToShoppingList shoppingList waypoints
 
     Assert.AreEqual(expected, result)
