8e1bbbcf2564b3c161e3c1db7d2fee75d1d7856c
Max <maksymilianrozanski@gmail.com>
2021-01-24 15:51:57 +0100

    add Dockerfile add web service to docker-compose.yml upgrade docker-compose.yml version to 3.9 use db as Server in database connection string

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+run_cmd="dotnet run --project ShoppingList"
+# --server.urls http://*:5000"
+
+until dotnet ef database update --project ShoppingList; do
+>&2 echo "SQL Server is starting up"
+sleep 1
+done
+
+>&2 echo "SQL Server is up - executing command"
+exec $run_cmd
