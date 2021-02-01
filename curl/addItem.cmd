546ec13554d190b5a507fa7f82177dfebcce6dd3
Max <maksymilianrozanski@gmail.com>
2021-02-01 18:20:32 +0100

    remove curl scripts

--- curl/addItem.cmd
+++
@@ -1,2 +0,0 @@
-rem requests adding new item to shopping list
-curl -X POST --user user:winter "localhost:5000/shoppingList/addItem" -d "@json/addItem2.json" -H "Content-Type: application/json" -v --ssl-no-revoke
--- curl/get2.cmd
+++
@@ -1,2 +0,0 @@
-rem requests shopping list from secured endpoint
-curl -X GET --user user:password "localhost:5000/shoppingList/2" -v --ssl-no-revoke
