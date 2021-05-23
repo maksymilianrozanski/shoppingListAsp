351e0681a6dc592fdea02c75c0e3d2e8a25043dd
Max <maksymilianrozanski@gmail.com>
2021-05-23 17:12:01 +0200

    add type Hash, change return type of createSha

--- Authentication.Tests/SaltTests.fs
+++ Authentication.Tests/SaltTests.fs
@@ -7,14 +7,19 @@ open SaltModule
 [<SetUp>]
 let Setup () = ()
 
+let salt = Salt "PasswordSalt"
+
+let password = Password "password"
+
 [<Test>]
 let shouldGenerateSha () =
-    let salt = Salt "PasswordSalt"
-
-    let password = Password "password"
-
     let expected =
-        "296BA429BD9F8B773D986C66C1D4AEEB659B442784D8F4E8752A3B6286997AAC"
+        Hash "296BA429BD9F8B773D986C66C1D4AEEB659B442784D8F4E8752A3B6286997AAC"
 
     let result = createSha salt password
     Assert.AreEqual(expected, result, "should create SHA with expected value")
+
+//[<Test>]
+//let ``should return true for equal saved and generated SHA`` () =
+//    let shaCreator salt password =
+//        ""
--- Authentication/Salt.fs
+++ Authentication/Salt.fs
@@ -9,12 +9,23 @@ module SaltModule =
     type Salt = Salt of string
     type Password = Password of string
 
+    type Hash = Hash of string
+
     let private joinSaltPassword salt password =
         match (salt, password) with
         | Salt s, Password p -> s + p
 
-    let createSha (salt: Salt) (password: Password) : string =
+    let createSha (salt: Salt) (password: Password) : Hash =
         joinSaltPassword salt password
         |> Encoding.UTF8.GetBytes
         |> SHA256.Create().ComputeHash
         |> Convert.ToHexString
+        |> Hash
+
+//    let verifySha (salt: Salt) (savedSha: Hash) (password: Password) =
+//        match (savedSha, createSha salt password) with
+//        | Hash a, b -> a = b
+//
+//    let verifySha2 (salt: Salt) shaCreator (savedSha: Hash) (password: Password) =
+//        match (savedSha, shaCreator salt password) with
+//        | Hash a, b -> a = b
